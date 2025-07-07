d2f645599cf8849dcea15ce16411e51bd765e40a
Alex Matcov <alex.matcov@gmail.com>
2025-07-07 22:14:00 +0200

    implement main logic, config, handling errors in main()

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
+use std::env;
+use std::fs;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    // store the collected values produced by the iterator into a vector
+    let args: Vec<String> = env::args().collect();
+    
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+
+    let contents = fs::read_to_string(config.file_path)
+        .expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
 }
 
 struct Config {
